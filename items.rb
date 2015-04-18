32775cd7f382d94e82ba2fe8a5cba03ef0d1bc0f
Damian Lajara <damiansito93@gmail.com>
2015-04-18 00:12:44 -0400

    Added Armor and Weapon class

--- items.rb
+++ items.rb
@@ -18,3 +18,20 @@ class Potion < Item
     @health = health
   end
 end
+
+class Weapon < Item
+  attr_reader :damage
+  #include List_of_weapons
+  def initialize(name, damage=0, price=0, sell_value=0)
+    super(name, price, sell_value)
+    @damage = damage
+  end
+end
+
+class Armor < Item
+  attr_reader :defense
+  def initialize(name, defense=0, price=0, sell_value=0)
+    super(name, price, sell_value)
+    @defense = defense
+  end
+end
