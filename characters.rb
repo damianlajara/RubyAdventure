ac12319bcc0b52f8bb00de956a3b2a8c9e13dca8
Damian Lajara <damiansito93@gmail.com>
2015-04-18 00:18:07 -0400

    Added ways for the user to customize his or her character

--- characters.rb
+++ characters.rb
@@ -18,4 +18,43 @@ class Character
     @money = money
     @experience = exp
   end
+
+  def customize_name
+    print "What would you like your character to be called? "
+    @name = gets.chomp.capitalize
+  end
+
+  def customize_gender
+    print "What is your gender? 1) #{GENDER[0]} 2) #{GENDER[1]} 3) Other "
+    @gender = gets.chomp.to_i
+    case @gender
+      when 1
+        @gender = GENDER[0]
+      when 2
+        @gender = GENDER[1]
+      when 3
+        print "Enter your preferred gender: "
+        @gender = gets.chomp.downcase
+      else
+        error
+    end
+  end
+
+  def customize_class
+    print "What class Would you like to choose your character from? 1) #{CLASSES[0]} 2) #{CLASSES[1]} 3) #{CLASSES[2]} "
+    @class = gets.chomp.to_i
+    case @class
+      when 1
+        #@figher.each { |weapon|  }
+        @class = CLASSES[0]
+      when 2
+        @class = CLASSES[1]
+
+      when 3
+        @class = CLASSES[2]
+
+      else
+        error
+    end
+  end
 end
