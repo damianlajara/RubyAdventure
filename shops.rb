3315ef3e60ca2df11eb0bfad48b1d4d6bd3c6aae
Damian Lajara <damiansito93@gmail.com>
2015-04-18 00:37:28 -0400

    Added a specific shop for the different types of items

--- shops.rb
+++ shops.rb
@@ -29,3 +29,145 @@ class Shop
   end
 
 end
+
+class SoldierWeaponShop < Shop
+  include SoldierWeaponAttributes
+
+  def initialize
+    @soldier_weapons = []
+    if $debug then puts "initializing soldier weapons" end
+    #@@soldier_weapon_names = %w[Meito Ichimonji Shusui Apocalypse Blade_of_Scars Ragnarok Eternal_Darkness Masamune Soul_Calibur]
+    effect = BASE_EFFECT
+    price = BASE_PRICE
+    sell_value = BASE_SELL_VALUE
+
+    @@soldier_weapon_names.length.times do |i|
+      effect += EFFECT_OFFSET
+      price += PRICE_OFFSET
+      sell_value += SELL_VALUE_OFFSET
+      @soldier_weapons.push(Weapon.new(@@soldier_weapon_names[i], effect, price, sell_value))
+    end
+  end
+
+  def display_weapons
+    @soldier_weapons.each(&Procs::DISPLAY)
+  end
+
+end
+
+  class MageWeaponShop < Shop
+    include MageWeaponAttributes
+    def initialize
+      @mage_weapons = []
+      if $debug then puts "initializing mage weapons" end
+      #@@mage_weapon_names = %w[Neil_Vajra Brionac Claimh_Solais Durandal Kusanagi Tizona Zulfiqar Orcrist]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@mage_weapon_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @mage_weapons.push(Weapon.new(@@mage_weapon_names[i], effect, price, sell_value))
+      end
+    end
+
+    def display_weapons
+      @mage_weapons.each(&Procs::DISPLAY)
+    end
+
+  end
+
+  class ArcherWeaponShop < Shop
+    include ArcherWeaponAttributes
+    def initialize
+      @archer_weapons = []
+      if $debug then puts "initializing archer weapons" end
+      #@@archer_weapon_names = %w[Arondight Gugnir Susano' Longinus Hrunting Clarent Shinigami Caliburn]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@archer_weapon_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @archer_weapons.push(Weapon.new(@@archer_weapon_names[i], effect, price, sell_value))
+      end
+    end
+
+    def display_weapons
+      @archer_weapons.each(&Procs::DISPLAY)
+    end
+
+  end
+
+#TODO
+  class SoldierArmorShop < Shop
+    def initialize
+      @soldier_armor = []
+      @@soldier_armor_names = %w[Calcite Mirage Djinn Shape_Shifter Dark_Prism Fatal_Sith Devastator Override]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@soldier_armor_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @soldier_armor.push(Armor.new(@@soldier_armor_names[i], effect, price, sell_value))
+      end
+    end
+  end
+
+  class MageArmorShop < Shop
+    def initialize
+      @mage_armor = []
+      @@mage_armor_names = %w[Colossus Eternal_Vanguard Prism Valkyrie Trident Eclipse Lunar_Spirit Astral_Inducer]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@mage_armor_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @mage_armor.push(Armor.new(@@mage_armor_names[i], effect, price, sell_value))
+      end
+    end
+  end
+
+  class ArcherArmorShop < Shop
+    def initialize
+      @archer_armor = []
+      @@archer_armor_names = %w[Nightmare Ashura Ichimonji Lionheart Ascalon Nirvana Chaotic_Axis Ominous_Judgement]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@archer_armor_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @archer_armor.push(Armor.new(@@archer_armor_names[i], effect, price, sell_value))
+      end
+    end
+  end
+
+  class PotionShop < Shop
+    def initialize
+      @potions = []
+      @@potion_names = %w[Mommy's_Tea Antidote_of_Life Red_Potion' Imperial_Regeneration Oil_of_Health Holy_Light Serum_of_Rejuvination Elixir]
+      effect = BASE_EFFECT
+      price = BASE_PRICE
+      sell_value = BASE_SELL_VALUE
+
+      @@potion_names.length.times do |i|
+        effect += EFFECT_OFFSET
+        price += PRICE_OFFSET
+        sell_value += SELL_VALUE_OFFSET
+        @potions.push(Potion.new(@@potion_names[i], effect, price, sell_value))
+      end
+    end
+  end
