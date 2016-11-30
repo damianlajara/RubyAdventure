128564dd436481df6322afd13c799b5fdd2ad45a
Damian Lajara <damian.lajara519@gmail.com>
2016-11-30 03:21:55 -0500

    add potion spec

--- lib/items/potion.rb
+++ lib/items/potion.rb
@@ -3,6 +3,7 @@ require_relative 'item'
 class Potion < Item
   attr_reader :health
   def initialize(name, item_args = {})
+    raise 'Invalid name entered' if name.empty? || !name.is_a?(String)
     super(name, item_args)
     @health = item_args[:health] || 0
   end
--- spec/lib/items/potion_spec.rb
+++ spec/lib/items/potion_spec.rb
@@ -1,16 +1,12 @@
-describe Armor do
-  subject(:armor) { Armor.new('a hat') }
+describe Potion do
+  subject(:potion) { Potion.new('a water bottle') }
   it 'inherits from Item' do
-    expect(Armor.superclass).to eq(Item)
+    expect(Potion.superclass).to eq(Item)
   end
 
   describe 'attributes' do
-    it 'has defense' do
-      expect(subject).to respond_to(:defense)
-    end
-    it 'has equipped' do
-      expect(subject).to respond_to(:equipped)
-      expect(subject).to respond_to(:equipped=)
+    it 'has health' do
+      expect(subject).to respond_to(:health)
     end
   end
 
@@ -19,8 +15,7 @@ describe Armor do
       expect(subject).to have_attributes(
         price: a_value >= 0,
         sell_value: a_value >= 0,
-        description: a_string_matching(''),
-        equipped: false
+        description: a_string_matching('')
       )
     end
 
@@ -29,36 +24,35 @@ describe Armor do
       context 'when name is passed in' do
         context 'when not empty' do
           it 'initializes with passed in name' do
-            expect(armor).to have_attributes(name: 'a hat')
+            expect(potion).to have_attributes(name: 'a water bottle')
           end
         end
 
         context 'when empty' do
           it 'raises error' do
-            expect { Armor.new('') }.to raise_error(invalid)
+            expect { Potion.new('') }.to raise_error(invalid)
           end
         end
       end
 
       context 'when only optional arguments are passed in' do
         it 'raises error' do
-          expect { Armor.new(defense: 250) }.to raise_error(invalid)
+          expect { Potion.new(health: 250) }.to raise_error(invalid)
         end
       end
 
       context 'when both name and optional arguments are passed in' do
         it 'initializes with passed in arguments' do
-          expect(Armor.new('a bat', defense: 500)).to have_attributes(defense: 500)
+          expect(Potion.new('a bat', health: 500)).to have_attributes(health: 500)
         end
         it 'overrides default values' do
-          expect(Armor.new('a ring',
-            defense: 500,
+          expect(Potion.new('soup',
+            health: 500,
             price: 40,
             sell_value: 20
           )).to have_attributes(
-            name: 'a ring',
-            equipped: false,
-            defense: 500,
+            name: 'soup',
+            health: 500,
             price: 40,
             sell_value: 20,
             description: ''
@@ -69,7 +63,7 @@ describe Armor do
 
     context 'when no arguments are passed in' do
       it 'raises argument error' do
-        expect { Armor.new }.to raise_error(ArgumentError)
+        expect { Potion.new }.to raise_error(ArgumentError)
       end
     end
   end
