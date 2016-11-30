e4cbb061fdc7b479d3a206aadedc96142a3f59c2
Damian Lajara <damian.lajara519@gmail.com>
2016-11-30 03:17:54 -0500

    add armor spec

--- lib/items/armor.rb
+++ lib/items/armor.rb
@@ -4,6 +4,7 @@ class Armor < Item
   attr_reader :defense
   attr_accessor :equipped
   def initialize(name, item_args = {})
+    raise 'Invalid name entered' if name.empty? || !name.is_a?(String)
     super(name, item_args)
     @equipped = false
     @defense = item_args[:defense] || 0
--- spec/lib/items/armor_spec.rb
+++ spec/lib/items/armor_spec.rb
@@ -1,12 +1,12 @@
-describe Weapon do
-  subject(:weapon) { Weapon.new('a knife') }
+describe Armor do
+  subject(:armor) { Armor.new('a hat') }
   it 'inherits from Item' do
-    expect(Weapon.superclass).to eq(Item)
+    expect(Armor.superclass).to eq(Item)
   end
 
   describe 'attributes' do
-    it 'has damage' do
-      expect(subject).to respond_to(:damage)
+    it 'has defense' do
+      expect(subject).to respond_to(:defense)
     end
     it 'has equipped' do
       expect(subject).to respond_to(:equipped)
@@ -29,36 +29,36 @@ describe Weapon do
       context 'when name is passed in' do
         context 'when not empty' do
           it 'initializes with passed in name' do
-            expect(weapon).to have_attributes(name: 'a knife')
+            expect(armor).to have_attributes(name: 'a hat')
           end
         end
 
         context 'when empty' do
           it 'raises error' do
-            expect { Weapon.new('') }.to raise_error(invalid)
+            expect { Armor.new('') }.to raise_error(invalid)
           end
         end
       end
 
       context 'when only optional arguments are passed in' do
         it 'raises error' do
-          expect { Weapon.new(damage: 250) }.to raise_error(invalid)
+          expect { Armor.new(defense: 250) }.to raise_error(invalid)
         end
       end
 
       context 'when both name and optional arguments are passed in' do
         it 'initializes with passed in arguments' do
-          expect(Weapon.new('a bat', damage: 500)).to have_attributes(damage: 500)
+          expect(Armor.new('a bat', defense: 500)).to have_attributes(defense: 500)
         end
         it 'overrides default values' do
-          expect(Weapon.new('a shuriken',
-            damage: 500,
+          expect(Armor.new('a shuriken',
+            defense: 500,
             price: 40,
             sell_value: 20
           )).to have_attributes(
             name: 'a shuriken',
             equipped: false,
-            damage: 500,
+            defense: 500,
             price: 40,
             sell_value: 20,
             description: ''
@@ -69,7 +69,7 @@ describe Weapon do
 
     context 'when no arguments are passed in' do
       it 'raises argument error' do
-        expect { Weapon.new }.to raise_error(ArgumentError)
+        expect { Armor.new }.to raise_error(ArgumentError)
       end
     end
   end
